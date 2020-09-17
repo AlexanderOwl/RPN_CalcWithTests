ebb9e39258b66dc417555bee437672b8525ba939
Олександр Owl <aischenko99@gmail.com>
2020-09-17 16:44:01 +0300

    Implement Main

--- RPN_CalcWithTests/Program.cs
+++ RPN_CalcWithTests/Program.cs
@@ -10,6 +10,11 @@ namespace RPN_CalcWithTests
     {
         static void Main(string[] args)
         {
+            while (true) //Бесконечный цикл
+            {
+                Console.Write("Input expression: "); //Предлагаем ввести выражение
+                Console.WriteLine(RPN.Calculate(Console.ReadLine())); //Считываем, и выводим результат
+            }
         }
     }
 }
