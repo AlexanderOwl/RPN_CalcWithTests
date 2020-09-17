2a6393da476f0ac651f170ec77488bbaf4c7c489
Олександр Owl <aischenko99@gmail.com>
2020-09-17 17:53:01 +0300

    Test on method IsDelimiter

--- NUnit.Tests1/TestClass.cs
+++ NUnit.Tests1/TestClass.cs
@@ -4,6 +4,8 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 
+using static RPN_CalcWithTests.RPN;
+using RPN_CalcWithTests;
 namespace NUnit.Tests
 {
     [TestFixture]
@@ -15,6 +17,15 @@ namespace NUnit.Tests
             // TODO: Add your test code here
             var answer = 42;
             Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+
+        [TestCase(' ', true, Description="Метод возвращает true, если проверяемый символ - разделитель (пробел)")]
+        [TestCase('=', true, Description="Метод возвращает true, если проверяемый символ - разделитель (равно)")]
+        [TestCase('1', false, Description="Метод возвращает false, если проверяемый символ - не разделитель (пробел или равно)")]
+        public void IsDelimiterReturnCorrectAnswer(char input , bool expected)
+        {
+           bool actual =  IsDelimeter(input);
+           Assert.AreEqual(expected, actual);
         }
     }
 }
--- RPN_CalcWithTests/RPN.cs
+++ RPN_CalcWithTests/RPN.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace RPN_CalcWithTests
 {
-    class RPN
+    public class RPN
     {
         //Метод возвращает true, если проверяемый символ - разделитель ("пробел" или "равно")
-        static private bool IsDelimeter(char c)
+        static public bool IsDelimeter(char c)
         {
             if ((" =".IndexOf(c) != -1))
                 return true;
             return false;
         }
-
         //Метод возвращает true, если проверяемый символ - оператор
         static private bool IsOperator(char с)
         {
