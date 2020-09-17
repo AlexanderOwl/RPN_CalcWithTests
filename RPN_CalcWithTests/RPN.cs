4babc4323b947d74d3e0a8efab82a9fa340e2ceb
Олександр Owl <aischenko99@gmail.com>
2020-09-17 16:07:11 +0300

    Create method Delimeter

--- RPN_CalcWithTests/RPN.cs
+++ RPN_CalcWithTests/RPN.cs
@@ -8,5 +8,14 @@ namespace RPN_CalcWithTests
 {
     class RPN
     {
+        //Метод возвращает true, если проверяемый символ - разделитель ("пробел" или "равно")
+        static private bool IsDelimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+
+        
     }
 }
